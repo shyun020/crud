141881fc616ceb2a2aaa3ae57e77cb0833a7014a
kimsoohyun <work.soohyun@gmail.com>
2023-12-04 17:03:17 +0900

    First commit

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import TopicList from '@/components/TopicList'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/signIn')
+  }
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold">WebDev Topics</h1>
+      <p>MongoDB CRUD Examples</p>
+      <TopicList />
+    </>
+  )
+}
